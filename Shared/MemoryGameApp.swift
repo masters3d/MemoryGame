893895ccfd64f62ed4b5b8916676e1b61cf37b24
Cheyo Jimenez <cheyo@masters3d.com>
2021-06-24 21:26:40 -0700

    Initial Commit

---
+++ Shared/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  Shared
+//
+//  Created by Cheyo Jimenez on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
