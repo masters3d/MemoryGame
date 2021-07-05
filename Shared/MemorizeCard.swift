784c9653479ab7bc0b3b89d5df445bf958608dbb
Chéyo Jiménez <masters3d@users.noreply.github.com>
2021-07-04 23:49:25 -0700

    July4th (#5)
    * added differnt size text for selection
    
    * added explicit size
    
    * added new game ability

--- Shared/MemorizeCard.swift
+++ Shared/MemorizeCard.swift
@@ -32,16 +32,27 @@ struct MemorizeCard: Identifiable, View {
 
     let cornerRadius = CGFloat(25)
     var body: some View {
+    GeometryReader { metrics in
         ZStack {
-            Image(uiImage: ImageCache[
-            "\(emoji.selectedCount < 1 ? "" : "\(emoji.selectedCount)" )"
-            ])
-            .resizable()
+            VStack {
+            HStack {
+                Image(uiImage: ImageCache[
+                "\(emoji.selectedCount < 1 ? "" : "\(emoji.selectedCount)" )"
+                ])
+                .resizable()
+            }.frame(width: metrics.size.width / (emoji.isMatch ? 1 : 3), height: metrics.size.height / (emoji.isMatch ? 1 : 3), alignment: .center)
+            Spacer(minLength: emoji.isMatch ? 0 : metrics.size.height * 0.65)
+            }
+            VStack {
+            Spacer(minLength: emoji.isMatch ? 0 : metrics.size.height * 0.10)
+
             Image(uiImage: emoji.isFaceUp && !emoji.isMatch ? emoji.emojiToImage() : ImageCache[""])
             .resizable()
+            }
         }
             .animation(.linear)
             .background(emoji.isMatch ? Color.green : Color.orange)
+            .frame(width: width, height: height, alignment: Alignment.center)
             .onDisappear {
             }
             .onTapGesture {
@@ -79,8 +90,11 @@ struct MemorizeCard: Identifiable, View {
             )
             .zIndex(0)
             .transition(.slide)// Slide seems to only work when there is more than one thing happening
-
+        }
     }
+
+    var width:Double
+    var height:Double
 
     var index:Int
     var emoji:EmojiState {
@@ -98,36 +112,34 @@ struct MemorizeCard: Identifiable, View {
     init(
         index: Int,
         matched: Binding<Dictionary<EmojiValue, Int>>,
-        current: Binding<Array<EmojiState>>
+        current: Binding<Array<EmojiState>>,
+        width: Double,
+        height: Double
         )
         {
             self.id = UUID()
             self.index = index
             self._matchedCards = matched
             self._currentList = current
+            self.width = width
+            self.height = height
         }
 }
 
 #if DEBUG
 struct MemorizeCard_Previews_Wrapper:View {
-    @State var current = Array<EmojiState>()
+    @State var current:Array<EmojiState> =  [EmojiState.init(value: EmojiValue.init(emojiAsString: "😎"))]
     @State var matched = Dictionary<EmojiValue, Int>()
     var body: some View {
-           current.append(EmojiState.init(value: EmojiValue.init(emojiAsString: "😎")))
 
-            return  MemorizeCard(index: 0, matched: $matched, current: $current)
-            .frame(width: 150, height: 150, alignment: .center)
+            return  MemorizeCard(index: 0, matched: $matched, current: $current, width: 200, height: 200)
     }
 }
 
 struct MemorizeCard_Previews: PreviewProvider {
     static var previews: some View {
-        if #available(iOS 15.0, *) {
-                MemorizeCard_Previews_Wrapper()
-                .previewInterfaceOrientation(.landscapeRight)
-        } else {
-            // Fallback on earlier versions
-        }
+       MemorizeCard_Previews_Wrapper()
+
     }
 }
 #endif
--- Shared/MemorizeGrid.swift
+++ Shared/MemorizeGrid.swift
@@ -20,6 +20,42 @@ struct MemorizeGrid: View {
     static var widthCount = 4
     static var heightCount = 5
 
+    @State private var isShowingWonAlert = false
+
+    func updateMatchedStatus() {
+        var selected = Dictionary<EmojiValue,Int>()
+
+        for each in currentList where each.isFaceUp {
+           if let value = selected[each.value] {
+                selected[each.value] = value + 1
+           } else {
+                selected[each.value] = 1
+           }
+        }
+
+        for (key, value) in selected where value > 1 {
+            matchedCards[key] = value
+            for (index,each) in currentList.enumerated() where
+                each.value.emojiAsString == key.emojiAsString
+            {
+                var each = each
+                each.isMatch = true
+                each.isFaceUp = false
+                    currentList[index] = each
+            }
+        }
+    }
+
+    func checkAndAlertWhenDone() {
+        let matchedCount = currentList.filter { emojiState in
+            emojiState.isMatch
+        }.count
+
+        if matchedCount == currentList.count {
+            isShowingWonAlert = true
+        }
+    }
+
     static func createEmoji() -> [EmojiState] {
 
         var temp = [EmojiState]()
@@ -44,40 +80,31 @@ struct MemorizeGrid: View {
                 HStack(spacing: 0) {
                     ForEach(0..<MemorizeGrid.widthCount) { widthIndex in
                         let emoji_index = (heightIndex * MemorizeGrid.widthCount) +  widthIndex
-                        let emojiView = MemorizeCard(index: emoji_index, matched: $matchedCards, current: $currentList)
-                        emojiView.padding(2)
+                        let emojiView = MemorizeCard(index: emoji_index, matched: $matchedCards, current: $currentList,
+                        width: (Double(geometry.size.width - geometry.safeAreaInsets.leading - geometry.safeAreaInsets.trailing) / Double(MemorizeGrid.widthCount)) - 4,
+                        height: (Double(geometry.size.height - geometry.safeAreaInsets.top - geometry.safeAreaInsets.bottom) / Double(MemorizeGrid.heightCount)) - 4
+                        )
+                        emojiView
                         }
                     }
                 }
             }
-        }.onReceive(timerMatchedCard) {
+        }.padding(4)
+        .alert(isPresented: $isShowingWonAlert) { () -> Alert in
+
+            let button = Alert.Button.default(Text("New Game")) {
+                currentList = MemorizeGrid.createEmoji()
+                isShowingWonAlert = false
+            }
+            return Alert(title: Text("You Won"), message: Text("Your score is: \(currentList.map {$0.selectedCount}.reduce(0, +))"), dismissButton: button)
+        }
+        .onReceive(timerMatchedCard) {
         _ in
-            var selected = Dictionary<EmojiValue,Int>()
-
-            for each in currentList where each.isFaceUp {
-               if let value = selected[each.value] {
-                    selected[each.value] = value + 1
-               } else {
-                    selected[each.value] = 1
-               }
-            }
-
-            for (key, value) in selected where value > 1 {
-                matchedCards[key] = value
-                for (index,each) in currentList.enumerated() where
-                    each.value.emojiAsString == key.emojiAsString
-                {
-                    var each = each
-                    each.isMatch = true
-                    each.isFaceUp = false
-                        currentList[index] = each
-                }
-            }
+            self.updateMatchedStatus()
+            self.checkAndAlertWhenDone()
         }
     }
 }
-
-
 
 #if DEBUG
 
